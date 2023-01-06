111b00e49be8781c57231abba34fca92fe936d83
Chandrabhan Rahangdale <nikhilrahangdale786@gmail.com>
2023-01-06 16:55:33 +0530

    final added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,39 @@ import "./App.css";
 import Imgbasketball from "./basketball.gif";
 
 function App() {
+
+  const[scoreA, setScoreA]= useState(0);
+  const[scoreB, setScoreB]= useState(0);
+
+  useEffect(()=>{
+    const a = localStorage.getItem("scoreA");
+    const b = localStorage.getItem("scoreB");
+    if(a){
+      setScoreA(a);
+    }
+    if(b){
+      setScoreB(b);
+    }
+  } , []) 
+
+  useEffect(()=>{
+    if(scoreA > 0)
+    {
+      localStorage.setItem("scoreA",scoreA);
+    }
+
+    if(scoreB > 0)
+    {
+      localStorage.setItem("scoreB" , scoreB);
+    }
+  },[scoreA,scoreB])
+
+  function resetScore(){
+    setScoreA(0);
+    setScoreB(0);
+    localStorage.clear();
+  }
+
   return (
     <>
       <div className="score-keeper-container">
@@ -11,13 +44,26 @@ function App() {
 
         <div className="score-card-container">
         <div className="score-card">
-          Team-A
+          <h3 className="score-card-heading">Team-A</h3>
+          <h6 className="score-count">{scoreA}</h6>
+          <div className="button-container">
+            <button className="score-button" onClick={()=>{setScoreA(scoreA+1)}}>+</button>
+            <button className="score-button" onClick={()=>{setScoreA(scoreA-1)}}>-</button>
+          </div>
         </div>
         <div className="score-card">
-          Team-B
+          <h3 className="score-card-heading">Team-B</h3>
+          <h6 className="score-count">{scoreB}</h6>
+          <div className="button-container">
+            <button className="score-button" onClick={()=>{setScoreB(scoreB+1)}}>+</button>
+            <button className="score-button" onClick={()=>{setScoreB(scoreB-1)}}>-</button>
+          </div>
         </div>
         </div>
 
+        <div>
+          <button className="reset-button"onClick={resetScore}>reset</button>
+        </div>
 
       </div>
     </>
